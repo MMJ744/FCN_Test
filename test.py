76eec44e0cdbb2ef9a2ca9de91ec42250a87231e
Matthew Jones <mattymjones@gmail.com>
2019-11-27 01:42:18 +0000

    working well on test.h2 trained on 250 epochs

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from keras import Input, Model
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ReduceLROnPlateau
 from matplotlib import image
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing.image import load_img
@@ -19,59 +19,9 @@ from keras.layers import Dropout, Flatten, Dense
 from keras.layers.convolutional import Deconvolution2D, Conv2DTranspose, Conv2D
 from keras.layers.merge import concatenate
 from keras import backend
+from tensorflow.python.client import device_lib
 
-def cnn_model():
-    # Channels first tells the pooling layer to use the (Height, Width, Depth) format instead of the (Depth, Height, Width)
-    data_format="channels_first"
-    model = Sequential()
-    model.add(Convolution2D(32, (3, 3), input_shape=(150, 150, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(data_format=data_format, pool_size=(2, 2)))
-    model.add(Convolution2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(data_format=data_format, pool_size=(2, 2)))
-    model.add(Convolution2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(data_format=data_format, pool_size=(2, 2)))
-    model.add(Flatten())
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1))
-    model.add(Activation('sigmoid'))
-    model.compile(optimizer='rmsprop',              # Performs gradient descent, finding the lowest error value
-                  loss='binary_crossentropy',       # Cross entropy measures the performance of each prediction made by the network
-                  metrics=['accuracy'])
-
-    return model
-
-def create_model():
-    # Channels first tells the pooling layer to use the (Height, Width, Depth) format instead of the (Depth, Height, Width)
-    data_format="channels_first"
-    # A sequential model is a basic model structure where the layers are stacked layer by layer.
-    # Another option with keras is a functional model, layers can be connected to literally any other layer within the model.
-    model = Sequential()
-    # A convolutional layer slides a filter over the image which is fed to the activation layer so the model can learn
-    # features and activate when they see one of these visual features. Only activated features are carried over to the
-    # next layer.
-    model.add(Conv2D(32, (3, 3), input_shape=(256, 256, 3),))
-    # Relu maps all negative values to 0 and keeps all positive values.
-    model.add(Activation('relu'))
-    # A pooling layer reduces the dimensions of the image but not the depth. The computation is faster and less image data
-    # means less chance of over fitting.
-    model.add(MaxPooling2D(data_format=data_format, pool_size=(2, 2)))
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(data_format=data_format, pool_size=(2, 2)))
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(data_format=data_format, pool_size=(2, 2)))
-    model.add(Conv2DTranspose(1, (3, 3), output_shape=(256,256)))
-    model.compile(optimizer='rmsprop',              # Performs gradient descent, finding the lowest error value
-                  loss='binary_crossentropy',       # Cross entropy measures the performance of each prediction made by the network
-                  metrics=['accuracy'])
-    print(model.summary())
-    return model
+print("-------------")
 
 def mean_iou(y_true, y_pred):
     prec = []
@@ -84,10 +34,16 @@ def mean_iou(y_true, y_pred):
         prec.append(score)
     return backend.mean(backend.stack(prec), axis=0)
 
+def dice_coef(y_true, y_pred, smooth=1):
+  intersection = backend.sum(y_true * y_pred, axis=[1,2,3])
+  union = backend.sum(y_true, axis=[1,2,3]) + backend.sum(y_pred, axis=[1,2,3])
+  dice = backend.mean((2. * intersection + smooth)/(union + smooth), axis=0)
+  return dice
 
 def unet_model():
-    inputs = Input((256,256,3))
+    inputs = Input((256, 256, 3))
     s = Lambda(lambda x: x / 255)(inputs)
+
     c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(s)
     c1 = Dropout(0.1)(c1)
     c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same')(c1)
@@ -149,13 +105,15 @@ test_size = 500
 train = []
 truth = []
 path = 'data/train/'
-for file in os.listdir(path):
+dir = sorted(os.listdir(path))
+for file in dir:
     img = image.imread(path+file)
     train.append(img/255)
 print(str(len(train))+ " images loaded for training")
 print(np.asarray(train).shape)
 path = 'data/train_truth/'
-for file in os.listdir(path):
+dir = sorted(os.listdir(path))
+for file in dir:
     img = image.imread(path+file)
     truth.append(img/255)
 print(str(len(truth))+ " Truth images loaded for training")
@@ -170,6 +128,36 @@ y_test = np.asarray(y[train_size:])
 y_test = y_test.reshape(y_test.shape + (1,))
 print(y_train.shape)
 print(y_test.shape)
-callbacks = [EarlyStopping(patience=10, verbose=1)]
-#model = create_model()
-model.fit(x=x_train,y=y_train,batch_size=batch_size,epochs=5,validation_data=(x_test,y_test),callbacks = callbacks)
+callbacks = [EarlyStopping(patience=10, verbose=1,monitor='val_loss'),ReduceLROnPlateau(patience=5, verbose=1,monitor='val_loss')]
+WEIGHTS_FILE = 'test.h5'
+if(False):
+    model.fit(x=x_train,y=y_train,batch_size=batch_size,epochs=1,validation_data=(x_test,y_test),callbacks = callbacks)
+    model.save_weights(WEIGHTS_FILE)
+else:
+    model.load_weights(WEIGHTS_FILE)
+#print(model.evaluate(x_test,y_test))
+print("--------------")
+img = np.asarray(train[120])
+#plt.imshow(img)
+#plt.show()
+plt.imshow(truth[120])
+plt.show()
+img = img.reshape((1,) + img.shape)
+print('**'+str(img.shape))
+output = model.predict(img)[0]
+print(output.min())
+print(output.max())
+outputround = output.copy()
+cutoff = 0.6
+outputround[outputround > cutoff] = 1
+outputround[outputround <= cutoff] = 0
+outputround = cv2.merge((outputround,outputround,outputround))
+print(outputround.min())
+print(outputround.max())
+print(outputround.shape)
+
+output = cv2.merge((output,output,output))
+plt.imshow(outputround, interpolation='nearest')
+plt.show()
+plt.imshow(output, interpolation='nearest')
+plt.show()
