9f7d215dc66f8aa18b695c1995ccf95141fa4eea
Matthew Jones <mattymjones@gmail.com>
2020-01-26 23:37:57 +0000

    restructuring and starting to work on brats data

--- brats.py
+++ brats.py
@@ -2,6 +2,8 @@ import cv2
 import os
 import numpy as np
 from matplotlib import image
+import models
+import matplotlib.pyplot as plt
 import nibabel
 
 def load_data():
@@ -11,6 +13,68 @@ def load_data():
   dir = sorted(os.listdir(path))
   for file in dir:
     x = nibabel.load(path + file)
-    train.append(x)
-    print(x.shape)
-load_data()
+    data = x.get_fdata()
+    if 'seg' in file:
+      truth.append(data)
+    else:
+      train.append(data)
+  return train, truth
+
+x,y = load_data()
+test = x[3]
+print(test.shape)
+new = np.zeros((240,240))
+noneZero = False
+c = 0
+c1 = 0
+for a in test:
+  c2 = 0
+  for b in a:
+    new[c1][c2] = np.max(b)
+    c2 +=1
+  c1 +=1
+
+print(new.shape)
+for i in range(240):
+  if np.max(new[i]) != 0: #239-i
+    print(i)
+    break
+for i in range(240):
+  if np.max(new[239-i]) != 0: #239-i
+    print(i)
+    break
+new = np.swapaxes(new,0,1)
+for i in range(240):
+  if np.max(new[i]) != 0: #239-i
+    print(i)
+    break
+for i in range(240):
+  if np.max(new[239-i]) != 0:
+    print(i)
+    break
+new = np.swapaxes(new,0,1)
+cropped = new[48:192,40:221]
+plt.imshow(new)
+plt.show()
+plt.imshow(cropped)
+plt.show()
+print("------")
+for i in range(cropped.shape[0]):
+  if np.max(cropped[i]) != 0: #239-i
+    print(i)
+    break
+for i in range(cropped.shape[0]):
+  if np.max(cropped[cropped.shape[0]-1-i]) != 0: #239-i
+    print(i)
+    break
+cropped = np.swapaxes(cropped,0,1)
+for i in range(cropped.shape[0]):
+  if np.max(cropped[i]) != 0: #239-i
+    print(i)
+    break
+for i in range(cropped.shape[0]):
+  if np.max(cropped[cropped.shape[0]-1-i]) != 0:
+    print(i)
+    break
+
+print(cropped.shape)
